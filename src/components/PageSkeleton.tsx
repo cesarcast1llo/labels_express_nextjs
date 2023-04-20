334b9630ef7f616aa3a8a02e582a9f8c27a91108
cesarcast1llo <cesarcast1llo@yahoo.com>
2023-04-19 19:52:23 -0700

    added vercel analytics

--- src/components/PageSkeleton.tsx
+++ src/components/PageSkeleton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Menu from '@/components/Menu';
+import { Analytics } from '@vercel/analytics/react';
 
 type PageSkeletonProps = React.PropsWithChildren<{
     children: React.ReactNode;
@@ -17,6 +18,7 @@ const PageSkeleton: React.FC<PageSkeletonProps> = ({ children, firstPage, second
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Analytics />
             <div className="labelsExpress">
                 <div className="wrapper">
                     <Menu firstPage={firstPage} secondPage={secondPage} />
