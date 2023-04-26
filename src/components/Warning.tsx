e96b88a205a723aae95df5a78958e62597b562f9
cesarcast1llo <cesarcast1llo@yahoo.com>
2023-04-26 10:49:47 -0700

    warning message added

---
+++ src/components/Warning.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Warning = () => {
+    return (
+        <>
+            <div className="warning">
+                <div className="warnWrapper">
+                    <p>UPS API is currently down, USPS is live</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Warning;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Form from '@/components/Form';
 import Prices from '@/components/Prices';
 import PageSkeleton from '@/components/PageSkeleton';
+import Warning from '@/components/Warning';
 
 export default function Home() {
     return (
         <PageSkeleton firstPage="Payment" secondPage="FAQ">
+            <Warning />
             <Prices />
             <Form />
         </PageSkeleton>
