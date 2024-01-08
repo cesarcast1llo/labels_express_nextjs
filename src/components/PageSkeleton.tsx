eb59c4a5f8d1788f300acb0a087c13cb6bbf5dff
cesarcast1llo <cesarc@contractor.indeed.com>
2024-01-07 22:56:35 -0800

    rm analytics

--- src/components/PageSkeleton.tsx
+++ src/components/PageSkeleton.tsx
@@ -17,7 +17,6 @@ const PageSkeleton: React.FC<PageSkeletonProps> = ({ children, firstPage, second
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Analytics />
             <div className="labelsExpress">
                 <div className="wrapper">
                     <Menu firstPage={firstPage} secondPage={secondPage} />
